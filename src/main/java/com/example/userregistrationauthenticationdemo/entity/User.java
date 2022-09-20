b5ebd69a6445da1425cb51d86014af0cbcb992c2
Salex <123@13.com>
2022-09-20 11:32:48 +0300

    Filled User and Role classes

--- src/main/java/com/example/userregistrationauthenticationdemo/entity/User.java
+++ src/main/java/com/example/userregistrationauthenticationdemo/entity/User.java
@@ -1,4 +1,62 @@
 package com.example.userregistrationauthenticationdemo.entity;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_user")
+@Setter
+@Getter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 4, message = "Не меньше 4 знаков")
+    private String username;
+
+    @Size(min = 4, message = "Не меньше 4 знаков")
+    private String password;
+
+    @Transient
+    private String passwordConfirm;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
 }
