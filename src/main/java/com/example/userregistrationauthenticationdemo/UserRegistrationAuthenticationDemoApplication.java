c6e66ad3cbf9d7f4d627ded56c33623ba7f60a7c
Salex <123@13.com>
2022-09-20 11:02:18 +0300

    Init commit

---
+++ src/main/java/com/example/userregistrationauthenticationdemo/UserRegistrationAuthenticationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.userregistrationauthenticationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationAuthenticationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userregistrationauthenticationdemo/UserRegistrationAuthenticationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userregistrationauthenticationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationAuthenticationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
